9da285f4c4bf1e40f01b069c853fca697933bd65
smilejakdu <ash982416@gmail.com>
2022-03-10 13:47:37 +0900

    kakao login callback

--- src/database/repository/user.repository.ts
+++ src/database/repository/user.repository.ts
@@ -1,10 +1,11 @@
 import { EntityRepository, QueryRunner, Repository } from 'typeorm';
 import { LoginType, UsersEntity } from '../entities/UsersEntity';
-import { HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { UserFindOneOptions } from '../service/users.service';
 import bcrypt from 'bcrypt';
 import { transactionRunner } from '../../shared/common/transaction/transaction';
 import { isNil } from 'lodash';
+import axios from 'axios';
 
 @EntityRepository(UsersEntity)
 export class UserRepository extends Repository<UsersEntity>{
@@ -94,4 +95,48 @@ export class UserRepository extends Repository<UsersEntity>{
   // async kakaoSignUp(){
   //
   // }
+
+  async kakaoCallback(query: any, redirectURI: string): Promise<any> {
+    const client_id = process.env.AUTH_KAKAO_CLIENT_ID;
+    const url = 'https://kauth.kakao.com/oauth/token';
+    if (!query.code) {
+      throw new HttpException('올바르지 않은 로그인 url입니다.', 400);
+    }
+    const data = {
+      grant_type: 'authorization_code',
+      client_id: client_id,
+      redirect_uri: redirectURI,
+      code: query.code,
+    };
+    const token_res = await axios.post(url, data , {
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    const get_profile_url = 'https://kapi.kakao.com/v2/user/me';
+
+    const getProfileHeaders = {
+      Authorization: `Bearer ${token_res.data.access_token}`,
+    };
+
+    const profile_res = await axios.post(get_profile_url, null , {
+      headers: getProfileHeaders,
+    }).then(res=>{
+        console.log(res);
+      }).catch(error=>{
+        console.log(error);
+      });
+
+    if (!profile_res.data.id) {
+      throw new HttpException('Kakao login error',
+                                        HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return profile_res.data;
+  }
 }

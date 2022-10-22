fa1d9684d7e42d01beea6f196a0cc45c75c667ea
smilejakdu <ash982416@gmail.com>
2022-10-22 15:50:48 +0900

    add datasource

---
+++ src/dataSource.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT) as number,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  entities: ['src/**/*{.entity.ts}'],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  charset: 'utf8mb4',
+  synchronize: false,
+  logging: true,
+});
+
+export default dataSource;

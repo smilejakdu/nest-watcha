a1aae7fe15f7d231afde335efd910d39dab768f5
smilejakdu <ash982416@gmail.com>
2022-12-03 23:58:37 +0900

    update scheduler service

--- src/service/schedules.service.ts
+++ src/service/schedules.service.ts
@@ -1,39 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { getManager } from 'typeorm';
 import dayjs from 'dayjs';
+import { OrderEntity } from "../database/entities/Order/order.entity";
+import { UsersEntity } from "../database/entities/User/Users.entity";
 
 @Injectable()
 export class SchedulesService {
-
-  findDayjs(){
+  findDayjs() {
     const fromDate = dayjs(new Date()).subtract(1, 'day').format('YYYY-MM-DD 00:00:00');
     const toDate = dayjs(new Date()).subtract(1, 'day').format('YYYY-MM-DD 23:59:59');
     return {fromDate , toDate};
   }
+
   async findAccountMoney() {
     const {fromDate , toDate} = this.findDayjs();
-    return await getManager().query(
-      `
-        select IFNULL(sum(o.order_price), 0) sum_price
-        from orders o 
-            join movies m 
-                on m.id = orders.movieId
-            join movie_options mo 
-                on m.movieOptionId = mo.id
-        where o.createdAt between  ? and ?;
-    `,
-      [fromDate, toDate],
-    );
+    return OrderEntity.makeQueryBuilder()
+      .select('IFNULL(SUM(orders.order_price),0) as sum_price')
+      .innerJoin('orders.Movie', 'movie')
+      .innerJoin('movie.MovieOption', 'MovieOption')
+      .where('orders.createdAt between :forDate and :toDate', {forDate: fromDate, toDate: toDate})
   }
-
   async findNewUser() {
     const {fromDate , toDate} = this.findDayjs();
-    return await getManager().query(
-      `
-        select *
-        from users u
-        where u.createdAt between ? and ?;
-      `,[fromDate,toDate]
-    );
+    return UsersEntity.makeQueryBuilder()
+      .where('createdAt between :forDate and :toDate', {forDate: fromDate, toDate: toDate})
   }
 }
